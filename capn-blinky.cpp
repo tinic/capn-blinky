98651c319a99a07949953f4b9b9c0e4c25cb566d
Tinic Uro <turo@adobe.com>
2021-11-12 18:23:24 -0800

    WIP

--- capn-blinky.cpp
+++ capn-blinky.cpp
@@ -1,14 +1,331 @@
 
 #include <stdint.h>
 #include <memory.h>
-
-#include "fpm/fixed.hpp"
-#include "fpm/math.hpp"
+#include <algorithm>
 
 #include "stm32l0xx_hal.h"
 
 extern "C" SPI_HandleTypeDef hspi1;
 extern "C" DMA_HandleTypeDef hdma_spi1_tx;
+
+template<size_t fbits> class fixed32 {
+
+public: 
+
+    constexpr fixed32() {
+        raw = 0;
+    }
+
+    template <typename T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
+    constexpr inline explicit fixed32(T a) noexcept {
+        raw = static_cast<int32_t>(a << fbits);
+    }
+
+    template <typename T, typename std::enable_if<std::is_floating_point<T>::value>::type* = nullptr>
+    constexpr inline explicit fixed32(T a) noexcept {
+        raw = static_cast<int32_t>(a * float(1L << fbits));
+    }
+
+    template <typename T, typename std::enable_if<std::is_floating_point<T>::value>::type* = nullptr>
+    constexpr inline explicit operator T() const noexcept
+    {
+        return static_cast<T>(raw) / T(1L<<fbits);
+    }
+
+    template <typename T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
+    constexpr inline explicit operator T() const noexcept
+    {
+        return static_cast<T>(raw >> fbits);
+    }
+
+    constexpr fixed32 &operator=(const fixed32& other) = default;
+
+    constexpr fixed32 &operator+=(const fixed32 &b) {
+        this->raw += b.raw;
+        return *this;
+    }
+
+    constexpr fixed32 &operator-=(const fixed32 &b) {
+        this->raw -= b.raw;
+        return *this;
+    }
+
+    constexpr fixed32 &operator*=(const fixed32 &b) {
+        auto value = (int64_t(this->raw) * int64_t(b.raw)) >> (fbits - 1);
+        this->raw = value / 2 + (value & 1);
+        return *this;
+    }
+
+    constexpr fixed32 &operator/=(const fixed32 &b) {
+        auto value = (int64_t(this->raw) << (fbits + 1)) / b.raw;
+        this->raw = value / 2 + (value & 1);
+        return *this;
+    }
+
+    constexpr fixed32 &operator%=(const fixed32 &b) {
+        this->raw = this->raw % b.raw;
+        return *this;
+    }
+
+    constexpr fixed32 operator-() const {
+        return -raw;
+    }
+
+    constexpr fixed32 operator+() const {
+        return +raw;
+    }
+
+    constexpr fixed32 operator+(const fixed32 &b) const {
+        fixed32 ret; 
+        ret.raw = raw + b.raw;
+        return ret;
+    }
+
+    constexpr fixed32 operator-(const fixed32 &b) const {
+        fixed32 ret; 
+        ret.raw = raw - b.raw;
+        return ret;
+    }
+
+    constexpr fixed32 operator*(const fixed32 &b) const {
+        auto value = (int64_t(this->raw) * int64_t(b.raw)) >> (fbits - 1);
+        fixed32 ret; 
+        ret.raw = value / 2 + (value & 1);
+        return ret;
+    }
+
+    constexpr fixed32 operator/(const fixed32 &b) const {
+        auto value = (int64_t(this->raw) << (fbits + 1)) / b.raw;
+        fixed32 ret; 
+        ret.raw = value / 2 + (value & 1);
+        return ret;
+    }
+
+    constexpr fixed32 operator%(const fixed32 &b) const {
+        fixed32 ret; 
+        ret.raw = raw % b.raw;
+        return ret;
+    }
+
+    int32_t raw_value() const { return raw; }
+
+private:
+
+    int32_t raw;
+};
+
+template <size_t fbits> constexpr inline bool operator==(const fixed32<fbits>& x, const fixed32<fbits>& y) noexcept {
+    return x.raw_value() == y.raw_value();
+}
+
+template <size_t fbits> constexpr inline bool operator!=(const fixed32<fbits>& x, const fixed32<fbits>& y) noexcept {
+    return x.raw_value() != y.raw_value();
+}
+
+template <size_t fbits> constexpr inline bool operator<(const fixed32<fbits>& x, const fixed32<fbits>& y) noexcept {
+    return x.raw_value() < y.raw_value();
+}
+
+template <size_t fbits> constexpr inline bool operator>(const fixed32<fbits>& x, const fixed32<fbits>& y) noexcept {
+    return x.raw_value() > y.raw_value();
+}
+
+template <size_t fbits> constexpr inline bool operator<=(const fixed32<fbits>& x, const fixed32<fbits>& y) noexcept {
+    return x.raw_value() <= y.raw_value();
+}
+
+template <size_t fbits> constexpr inline bool operator>=(const fixed32<fbits>& x, const fixed32<fbits>& y) noexcept {
+    return x.raw_value() >= y.raw_value();
+}
+
+class hsv;
+
+class rgb {
+public:
+	fixed32<24> r;
+	fixed32<24> g;
+	fixed32<24> b;
+
+	constexpr rgb() :
+		r(0.0f),
+		g(0.0f),
+		b(0.0f){
+	}
+
+	rgb(const rgb &from) :
+		r(from.r),
+		g(from.g),
+		b(from.b) {
+	}
+
+	explicit rgb(const uint32_t color) {
+        r = static_cast<fixed32<24>>((color>>16)&0xFF) * fixed32<24>(1.0f/255.0f);
+        g = static_cast<fixed32<24>>((color>> 8)&0xFF) * fixed32<24>(1.0f/255.0f);
+        b = static_cast<fixed32<24>>((color>> 0)&0xFF) * fixed32<24>(1.0f/255.0f);
+	}
+	
+	explicit rgb(const hsv &from);
+
+    constexpr rgb &operator=(const rgb& other) = default;
+
+	constexpr rgb(fixed32<24> _r, fixed32<24> _g, fixed32<24> _b) :
+		r(_r),
+		g(_g),
+		b(_b) {
+	}
+
+	void set(fixed32<24> _r, fixed32<24> _g, fixed32<24> _b) {
+		r = _r;
+		g = _g;
+		b = _b;
+	}
+
+	rgb &operator+=(const rgb &v) {
+		r += v.r;
+		g += v.g;
+		b += v.b;
+		return *this;
+	}
+
+	friend rgb operator+(rgb a, const rgb &_b) {
+		a += _b;
+		return a;
+	}
+
+	rgb &operator-=(const rgb &v) {
+		r -= v.r;
+		g -= v.g;
+		b -= v.b;
+		return *this;
+	}
+
+	friend rgb operator-(rgb a, const rgb &_b) {
+		a -= _b;
+		return a;
+	}
+
+	rgb &operator*=(const rgb &v) {
+		r *= v.r;
+		g *= v.g;
+		b *= v.b;
+		return *this;
+	}
+
+	friend rgb operator*(rgb a, const rgb &_b) {
+		a *= _b;
+		return a;
+	}
+
+	rgb &operator*=(fixed32<24> v) {
+		r *= v;
+		g *= v;
+		b *= v;
+		return *this;
+	}
+
+	friend rgb operator*(rgb a, fixed32<24> v) {
+		a *= v;
+		return a;
+	}
+
+	rgb &operator/=(const rgb &v) {
+		r /= v.r;
+		g /= v.g;
+		b /= v.b;
+		return *this;
+	}
+
+	friend rgb operator/(rgb a, const rgb &_b) {
+		a /= _b;
+		return a;
+	}
+
+	rgb &operator/=(fixed32<24> v) {
+		r /= v;
+		g /= v;
+		b /= v;
+		return *this;
+	}
+
+	friend rgb operator/(rgb a, fixed32<24> v) {
+		a /= v;
+		return a;
+	}
+};
+
+
+class hsv {
+public:
+	fixed32<24> h;
+	fixed32<24> s;
+	fixed32<24> v;
+
+	constexpr hsv() :
+		h(0.0f),
+		s(0.0f),
+		v(0.0f) {
+	}
+
+	constexpr hsv(fixed32<24> _h, fixed32<24> _s, fixed32<24> _v) :
+		h(_h),
+		s(_s),
+		v(_v) {
+	}
+
+	constexpr hsv(const hsv &from) :
+		h(from.h),
+		s(from.s),
+		v(from.v) {
+	}
+
+    constexpr hsv &operator=(const hsv& other) = default;
+
+	constexpr explicit hsv(const rgb &from) {
+		fixed32<24> hi = std::max(std::max(from.r, from.g), from.b);
+		fixed32<24> lo = std::min(std::max(from.r, from.g), from.b);
+		fixed32<24> d = hi - lo;
+
+		h = fixed32<24>(0.0f);
+		s = fixed32<24>(0.0f);
+		v = hi;
+
+		if ( ( v > fixed32<24>(0.00001f) ) &&
+			 ( d > fixed32<24>(0.00001f) ) ) {
+			s = d / v;
+			if( hi == from.r ) {
+				h = fixed32<24>(60.0f/360.0f) * (from.g - from.b) / d + (from.g < from.b ? fixed32<24>(1.0f) : fixed32<24>(0.0f));
+			}
+			if( hi == from.g ) {
+				h = fixed32<24>(60.0f/360.0f) * (from.b - from.r) / d + fixed32<24>(120.0f/360.0f);
+			}
+			if( hi == from.b ) {
+				h = fixed32<24>(60.0f/360.0f) * (from.r - from.g) / d + fixed32<24>(240.0f/360.0f);
+			}
+		}
+	}
+};
+
+rgb::rgb(const hsv &from) {
+	fixed32<24> v = from.v;
+	fixed32<24> h = from.h;
+	fixed32<24> s = from.s;
+
+	int32_t rd = static_cast<int32_t>( fixed32<24>(6.0f) * h );
+	fixed32<24> f = h * fixed32<24>(6.0f) - fixed32<24>(rd);
+	fixed32<24> p = v * (fixed32<24>(1.0f) - s);
+	fixed32<24> q = v * (fixed32<24>(1.0f) - f * s);
+	fixed32<24> t = v * (fixed32<24>(1.0f) - (fixed32<24>(1.0f) - f) * s);
+
+	switch ( rd  % 6 ) {
+		default:
+		case 0: r = v; g = t; b = p; break;
+		case 1: r = q; g = v; b = p; break;
+		case 2: r = p; g = v; b = t; break;
+		case 3: r = p; g = q; b = v; break;
+		case 4: r = t; g = p; b = v; break;
+		case 5: r = v; g = p; b = q; break;
+	}
+}
 
 class Leds {
 public:
@@ -19,10 +336,10 @@ public:
 
     void transfer();
 
+    static rgb led_buffer[ledsN * 3];
 
 private:
 
-    static uint16_t led_buffer[ledsN * 3];
     static uint8_t spi_buffer[ledsN * sizeof(uint16_t) * 3 * 4 + spiPaddingBytes];
 
     void init();
@@ -30,7 +347,7 @@ private:
 };
 
 uint8_t Leds::spi_buffer[ledsN * sizeof(uint16_t) * 3 * 4 + spiPaddingBytes];
-uint16_t Leds::led_buffer[ledsN * 3];
+rgb Leds::led_buffer[ledsN * 3];
 
 Leds &Leds::instance() {
     static Leds leds;
@@ -42,7 +359,6 @@ Leds &Leds::instance() {
 }
 
 void Leds::init() {
-    memset(led_buffer, 0, sizeof(led_buffer));
 }
 
 void Leds::transfer() {
@@ -61,7 +377,9 @@ void Leds::transfer() {
             *p++ = convert_half_to_spi((v>>0)&0xFF);
             return p;
         };
-        ptr = convert_to_one_wire_spi(ptr, led_buffer[c]);
+        ptr = convert_to_one_wire_spi(ptr, ((led_buffer[c].r.raw_value()-1)>>8)&0xFFFF);
+        ptr = convert_to_one_wire_spi(ptr, ((led_buffer[c].g.raw_value()-1)>>8)&0xFFFF);
+        ptr = convert_to_one_wire_spi(ptr, ((led_buffer[c].b.raw_value()-1)>>8)&0xFFFF);
     }
 
     for (size_t c = 0; c < spiPaddingBytes/(sizeof(uint32_t)*2); c++ ) {
@@ -74,7 +392,14 @@ void Leds::transfer() {
 }
 
 extern "C" void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *) {
+    static uint32_t tick = 0;
+    for (size_t c = 0; c < Leds::ledsN; c++) {
+        fixed32<24> h = fixed32<24>(1.0f) - fixed32<24>(tick & 0x1F);
+        fixed32<24> hue((h + fixed32<24>(c) * fixed32<24>(1.0f / 255.0f)) % fixed32<24>(1.0f));
+        Leds::led_buffer[c] = rgb(hsv(hue, fixed32<24>(1.0f), fixed32<24>(1.0f)));
+    }
     Leds::instance().transfer();
+    tick++;
 }
 
 extern "C" void HAL_GPIO_EXTI_Callback(uint16_t) {
